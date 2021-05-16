ab805469611d1c301f35a82dd5772b5faa467ce6
victor <victorolasupo25@gmail.com>
2021-05-16 17:54:49 +0100

    testCreateRubric() This test will test the Creation of a new Rubric Mocking the inputs given by the user: Rubric Name

--- CA2/src/main/java/Controller.java
+++ CA2/src/main/java/Controller.java
@@ -1,5 +1,17 @@
 public class Controller {
+    ArrayList<Rubric> rubrics;//ArrayList to store new rubrics
+	ArrayList<Student> students;//ArrayList to store new students
+	Scanner inputReader;
     public Controller() {
+        rubrics = new ArrayList<>(); 
+		students = new ArrayList<>(); 
+		inputReader = new Scanner(System.in);
     }
+
+    public void createRubric() {  // Method that will create a rubric when name of rubric is entered 
+		System.out.print("Enter Rubric Name: ");
+		Rubric rubric = new Rubric(inputReader.nextLine());
+		rubrics.add(rubric);
+	}
 }
 
